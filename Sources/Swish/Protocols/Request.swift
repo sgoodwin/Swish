0e02f59d5caa1a6979e49107237cee13dcc0c38d
Samuel Goodwin <samuel@roundwallsoftware.com>
2020-06-23 14:12:32 +0200

    Fix issue with JSON date parsing.
    The default behavior of the JSON Decoder is broken and has always been.
    This fixes it.

--- Sources/Swish/Protocols/Request.swift
+++ Sources/Swish/Protocols/Request.swift
@@ -11,14 +11,29 @@ public protocol Request {
 
 public extension Request where ResponseObject: Decodable {
   func parse<Wrapped>(_ data: Data) -> ResponseObject where ResponseObject == Wrapped? {
-    return try? JSONDecoder().decode(ResponseObject.self, from: data)
+      let decoder = JSONDecoder()
+      decoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601Full)
+      return try? decoder.decode(ResponseObject.self, from: data)
   }
 
   func parse<Wrapped>(_ data: Data) throws -> ResponseObject where ResponseObject == Wrapped {
-    return try JSONDecoder().decode(ResponseObject.self, from: data)
+      let decoder = JSONDecoder()
+      decoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601Full)
+      return try decoder.decode(ResponseObject.self, from: data)
   }
 }
 
 public extension Request where ResponseObject == EmptyResponse {
   func parse(_: Data) throws -> ResponseObject {}
+}
+
+extension DateFormatter {
+    static let iso8601Full: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
 }
